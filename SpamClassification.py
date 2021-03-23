7ec32669e364dba10900a9cabc201741237f7645
Sabri Mahmoud <80068228+SabriMahmoud@users.noreply.github.com>
2021-03-23 17:55:32 +0100

    Delete SpamClassification.py

--- SpamClassification.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np 
-import pandas as pd 
-import nltk
-#stopwords are : the is ,an ,witch..
-from nltk.corpus import stopwords
-import string
-from google.colab import files
-uploaded=files.upload()
